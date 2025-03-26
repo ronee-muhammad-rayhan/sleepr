52891ad32c3d291ff1f5e70014d57ac9dd3a12e8
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2025-03-26 16:22:58 +0600

    11 a logger

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,9 +4,21 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { LoggerModule } from 'nestjs-pino';
+import { single } from 'rxjs';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema },])],
+  imports: [DatabaseModule, DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema },]), LoggerModule.forRoot({
+    pinoHttp: {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          singleLine: true,
+        },
+      },
+    },
+  })],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
